d8a1624f38969e5e2b5a635c0b65032749943e1b
Kadi Kraman <hellokadi@gmail.com>
2020-03-05 16:46:19 -0600

    Navigation exercise 3

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const COLORS = [
+const SOLARIZED = [
   { colorName: 'Base03', hexCode: '#002b36' },
   { colorName: 'Base02', hexCode: '#073642' },
   { colorName: 'Base01', hexCode: '#586e75' },
@@ -21,19 +21,46 @@ const COLORS = [
   { colorName: 'Green', hexCode: '#859900' },
 ];
 
+const RAINBOW = [
+  { colorName: 'Red', hexCode: '#FF0000' },
+  { colorName: 'Orange', hexCode: '#FF7F00' },
+  { colorName: 'Yellow', hexCode: '#FFFF00' },
+  { colorName: 'Green', hexCode: '#00FF00' },
+  { colorName: 'Violet', hexCode: '#8B00FF' },
+];
+
+const FRONTEND_MASTERS = [
+  { colorName: 'Red', hexCode: '#c02d28' },
+  { colorName: 'Black', hexCode: '#3e3e3e' },
+  { colorName: 'Grey', hexCode: '#8a8a8a' },
+  { colorName: 'White', hexCode: '#ffffff' },
+  { colorName: 'Orange', hexCode: '#e66225' },
+];
+
+const COLOR_PALETTES = [
+  { paletteName: 'Solarized', colors: SOLARIZED },
+  { paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS },
+  { paletteName: 'Rainbow', colors: RAINBOW },
+];
+
 const Home = ({ navigation }) => {
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() =>
-          navigation.push('ColorPalette', {
-            paletteName: 'Solarized',
-            colors: COLORS,
-          })
-        }>
-        <Text>Solarized</Text>
-      </TouchableOpacity>
-    </View>
+    <FlatList
+      data={COLOR_PALETTES}
+      keyExtractor={item => item.paletteName}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.push('ColorPalette', {
+              paletteName: item.paletteName,
+              colors: item.colors,
+            })
+          }
+        >
+          <Text>{item.paletteName}</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
