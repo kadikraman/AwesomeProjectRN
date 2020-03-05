16847962f25d823c57d2f7a4c060e976ccc53df5
Kadi Kraman <hellokadi@gmail.com>
2020-03-05 16:37:56 -0600

    Navigation exercise 1

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -2,35 +2,18 @@ import React from 'react';
 import { Text, StyleSheet, FlatList } from 'react-native';
 import ColorBox from '../components/ColorBox';
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
+const ColorPalette = ({ route }) => {
+  const { paletteName, colors } = route.params;
 
-const ColorPalette = () => {
   return (
     <FlatList
       style={styles.container}
-      data={COLORS}
+      data={colors}
       keyExtractor={item => item.hexCode}
       renderItem={({ item }) => (
         <ColorBox hexCode={item.hexCode} colorName={item.colorName} />
       )}
-      ListHeaderComponent={<Text style={styles.heading}>Solarized</Text>}
+      ListHeaderComponent={<Text style={styles.heading}>{paletteName}</Text>}
     />
   );
 };
@@ -38,6 +21,7 @@ const ColorPalette = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    backgroundColor: 'white',
   },
   heading: {
     fontSize: 18,
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
 
 const Home = ({ navigation }) => {
   return (
     <View>
-      <TouchableOpacity onPress={() => navigation.push('ColorPalette')}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.push('ColorPalette', {
+            paletteName: 'Solarized',
+            colors: COLORS,
+          })
+        }>
         <Text>Solarized</Text>
       </TouchableOpacity>
     </View>
